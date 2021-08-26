2548f57ed4ed9bd01979bb1c60147f42d77f3e7c
HUSSAR-mtrela <martin.trela@gmail.com>
2021-08-26 10:15:50 -0400

    added test sample

---
+++ samples/charts/pie-chart/styling/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+// import { PieChartStylingComponent } from "./pie-chart-styling/pie-chart-styling.component";
+import {
+	IgxPieChartModule,
+	IgxLegendModule,
+	IgxItemLegendModule
+ } from "igniteui-angular-charts";
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent
+	// removed PieChartStylingComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPieChartModule,
+	IgxLegendModule,
+	IgxItemLegendModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
