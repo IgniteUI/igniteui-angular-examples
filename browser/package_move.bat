f22a0c5f870954bd37d4784f49aa48b67217df5d
HUSSAR-mtrela <martin.trela@gmail.com>
2022-06-02 16:00:14 -0400

    added v22.1.0-dev

---
+++ browser/package_move.bat
@@ -0,0 +1,40 @@
+@echo off
+ECHO moving IG development packages...
+CD node_modules
+
+IF EXIST @infragistics\igniteui-angular-charts (
+ MOVE /y @infragistics\igniteui-angular-charts igniteui-angular-charts
+) 
+
+IF EXIST @infragistics\igniteui-angular-core (
+ MOVE /y @infragistics\igniteui-angular-core igniteui-angular-core
+) 
+
+IF EXIST @infragistics\igniteui-angular-gauges (
+ MOVE /y @infragistics\igniteui-angular-gauges igniteui-angular-gauges
+) 
+
+IF EXIST @infragistics\igniteui-angular-grids (
+ MOVE /y @infragistics\igniteui-angular-grids igniteui-angular-grids
+)
+ 
+IF EXIST @infragistics\igniteui-angular-excel (
+ MOVE /y @infragistics\igniteui-angular-excel igniteui-angular-excel
+) 
+
+IF EXIST @infragistics\igniteui-angular-maps (
+ MOVE /y @infragistics\igniteui-angular-maps igniteui-angular-maps
+)
+
+IF EXIST @infragistics\igniteui-angular-spreadsheet (
+ MOVE /y @infragistics\igniteui-angular-spreadsheet igniteui-angular-spreadsheet
+)
+
+IF EXIST @infragistics\igniteui-angular-spreadsheet-chart-adapter (
+ MOVE /y @infragistics\igniteui-angular-spreadsheet-chart-adapter igniteui-angular-spreadsheet-chart-adapter
+)
+
+ECHO moving IG development packages... completed
+pause
+
+
