7cb72c97695c9c858e8e79183e4ce54bbbfbeb5f
HUSSAR-mtrela <martin.trela@gmail.com>
2022-06-10 11:04:40 -0400

    updated IG to v22.1.45

--- samples/charts/financial-chart/data-tooltip-formatting-currency/src/MultipleStocks.ts
+++ samples/charts/financial-chart/data-tooltip-formatting-currency/src/MultipleStocks.ts
@@ -2,16 +2,16 @@
 export class MultipleStocks extends Array<Array<StockItem>> {
     public static async fetch(): Promise<MultipleStocks> {
         const dataSources: any[] = [
-        //await this.getAmazonStock(),
-        await this.getGoogleStock(),
-        await this.getMicrosoftStock(),
-        //await this.getTeslaStock()
+          //await this.getAmazonStock(),
+          await this.getGoogleStock(),
+          await this.getAmazonStock(),
+          //await this.getTeslaStock()
         ];
         return new Promise<MultipleStocks>((resolve, reject) => {
         resolve(dataSources);
         });
     }
-  
+
     /** gets Amazon stock OHLC prices from a .JSON file */
     public static async getAmazonStock(): Promise<StockItem[]> {
       let url = "https://static.infragistics.com/xplatform/data/stocks/stockAmazon.json";
@@ -23,12 +23,12 @@ export class MultipleStocks extends Array<Array<StockItem>> {
         close: ["SeriesTitle/Amazon"]
       };
       // console.log("fetchAmazonStock: ", stockData.length);
-  
+
       return new Promise<StockItem[]>((resolve, reject) => {
         resolve(stockData);
       });
     }
-  
+
     /** gets Tesla stock OHLC prices from a .JSON file */
     public static async getTeslaStock(): Promise<StockItem[]> {
       let url = "https://static.infragistics.com/xplatform/data/stocks/stockTesla.json";
@@ -43,7 +43,7 @@ export class MultipleStocks extends Array<Array<StockItem>> {
         resolve(stockData);
       });
     }
-  
+
     /** gets Microsoft stock OHLC prices from a .JSON file */
     public static async getMicrosoftStock(): Promise<StockItem[]> {
       let url = "https://static.infragistics.com/xplatform/data/stocks/stockMicrosoft.json";
@@ -58,7 +58,7 @@ export class MultipleStocks extends Array<Array<StockItem>> {
         resolve(stockData);
       });
     }
-  
+
     /** gets Google stock OHLC prices from a .JSON file */
     public static async getGoogleStock(): Promise<StockItem[]> {
       let url = "https://static.infragistics.com/xplatform/data/stocks/stockGoogle.json";
@@ -73,10 +73,10 @@ export class MultipleStocks extends Array<Array<StockItem>> {
         resolve(stockData);
       });
     }
-  
+
     public static convertData(jsonData: any[]): StockItem[] {
       let stockItems: StockItem[] = [];
-  
+
       for (let json of jsonData) {
         let parts = json.date.split("-"); // "2020-01-01"
         let item = new StockItem();
@@ -87,21 +87,21 @@ export class MultipleStocks extends Array<Array<StockItem>> {
         item.close = json.close;
         item.volume = json.volume;
         stockItems.push(item);
-  
+
       }
-  
+
       return stockItems;
     }
   }
-  
+
   export class StockItem {
     public open?: number;
     public close?: number;
     public high?: number;
     public low?: number;
     public volume?: number;
-  
+
     public date?: Date;
-  
+
   }
 //end async data
