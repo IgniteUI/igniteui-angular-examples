201bdc1cfc77763c5b17745ce6860ebfb6948741
HUSSAR-mtrela <martin.trela@gmail.com>
2022-06-10 14:42:38 -0400

    exported samples

---
+++ samples/charts/category-chart/column-chart-with-tooltips/src/app.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, LegendDescriptionModule, CategoryChartDescriptionModule } from 'igniteui-angular-core';
+import { HighestGrossingMoviesItem, HighestGrossingMovies } from './HighestGrossingMovies';
+import { IgxPropertyEditorPanelComponent, IgxPropertyEditorPropertyDescriptionComponent } from 'igniteui-angular-layouts';
+import { IgxLegendComponent, IgxCategoryChartComponent } from 'igniteui-angular-charts';
+
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import { defineAllComponents } from 'igniteui-webcomponents';
+defineAllComponents();
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+
+    public constructor(private _detector: ChangeDetectorRef) {
+
+    }
+
+    @ViewChild("propertyEditor", { static: true } )
+    private propertyEditor: IgxPropertyEditorPanelComponent
+    @ViewChild("toolTipTypeEditor", { static: true } )
+    private toolTipTypeEditor: IgxPropertyEditorPropertyDescriptionComponent
+    @ViewChild("legend", { static: true } )
+    private legend: IgxLegendComponent
+    @ViewChild("chart", { static: true } )
+    private chart: IgxCategoryChartComponent
+
+    private _highestGrossingMovies: HighestGrossingMovies = null;
+    public get highestGrossingMovies(): HighestGrossingMovies {
+        if (this._highestGrossingMovies == null)
+        {
+            this._highestGrossingMovies = new HighestGrossingMovies();
+        }
+        return this._highestGrossingMovies;
+    }
+    
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            LegendDescriptionModule.register(context);
+            CategoryChartDescriptionModule.register(context);
+        }
+        return this._componentRenderer
+    }
+
+}
+
