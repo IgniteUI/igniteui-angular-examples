35786377dc3396a735191d340e98a4c69a3e1c3b
agoldenbaum <agoldenbaum@infragistics.com>
2022-08-03 17:30:44 -0400

    Modify the working-with-sparklines sample to use the IgxSparkline

--- samples/excel/excel-library/working-with-sparklines/src/app/app.module.ts
+++ samples/excel/excel-library/working-with-sparklines/src/app/app.module.ts
@@ -5,9 +5,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 import { IgxButtonModule, IgxGridModule } from "igniteui-angular";
-import { IgxCategoryChartModule, IgxDataChartCategoryModule, IgxDataChartCoreModule, IgxDataChartInteractivityModule } from "igniteui-angular-charts";
+import { IgxSparklineModule } from "igniteui-angular-charts";
 import { IgxExcelModule } from "igniteui-angular-excel";
-import { ExcelUtility } from "./ExcelUtility";
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -19,14 +18,11 @@ import { ExcelUtility } from "./ExcelUtility";
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
-    FormsModule,
-    IgxButtonModule,
-    IgxCategoryChartModule,
+    FormsModule,    
+    IgxButtonModule,    
     IgxGridModule,
-    IgxExcelModule,
-    IgxDataChartCategoryModule,
-    IgxDataChartCoreModule,
-    IgxDataChartInteractivityModule
+    IgxSparklineModule,
+    IgxExcelModule
 ],
   providers: [],
   entryComponents: [],
