05c0c52f1f3d0a4b6aedfb753fa24e1e587507f5
Michael DiFilippo <mdifilippo@infragistics.com>
2022-12-07 17:01:15 -0500

    mdd-update-Angular15
    update to Angular 15

--- browser/src/app/app-routing.module.ts
+++ browser/src/app/app-routing.module.ts
@@ -65,7 +65,7 @@ export const appRoutes: Routes = [
 ];
 @NgModule({
     exports: [RouterModule],
-    imports: [RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })]
+    imports: [RouterModule.forRoot(appRoutes)]
 })
 export class AppRoutingModule {
 
