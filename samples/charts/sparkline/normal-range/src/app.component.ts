40e1d23d7950e2fad27cac983b146e9d0ded1022
tfsbuild <tfsbuild@infragistics.com>
2023-04-13 12:12:45 +0300

    Adding changes from build IgniteUI.igniteui-xplat-examples_2023.4.13.10

--- samples/charts/sparkline/normal-range/src/app.component.ts
+++ samples/charts/sparkline/normal-range/src/app.component.ts
@@ -1,12 +1,9 @@
 import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ComponentRenderer, PropertyEditorPanelDescriptionModule, SparklineDescriptionModule } from 'igniteui-angular-core';
-import { SparklineMixedDataItem, SparklineMixedData } from './SparklineMixedData';
-import { IgxPropertyEditorPanelComponent, IgxPropertyEditorPropertyDescriptionComponent } from 'igniteui-angular-layouts';
-import { IgxSparklineComponent } from 'igniteui-angular-charts';
+//insert bindingImports
+//end bindingImports
 
-import { defineAllComponents } from 'igniteui-webcomponents';
 
-defineAllComponents();
 
 @Component({
     selector: "app-root",
@@ -20,26 +17,9 @@ export class AppComponent {
 
     }
 
-    @ViewChild("propertyEditorPanel1", { static: true } )
-    private propertyEditorPanel1: IgxPropertyEditorPanelComponent
-    @ViewChild("normalRangeVisibilityEditor", { static: true } )
-    private normalRangeVisibilityEditor: IgxPropertyEditorPropertyDescriptionComponent
-    @ViewChild("normalRangeMinimumEditor", { static: true } )
-    private normalRangeMinimumEditor: IgxPropertyEditorPropertyDescriptionComponent
-    @ViewChild("normalRangeMaximumEditor", { static: true } )
-    private normalRangeMaximumEditor: IgxPropertyEditorPropertyDescriptionComponent
-    @ViewChild("chart", { static: true } )
-    private chart: IgxSparklineComponent
+    //insert bindingFields
+    //end bindingFields
 
-    private _sparklineMixedData: SparklineMixedData = null;
-    public get sparklineMixedData(): SparklineMixedData {
-        if (this._sparklineMixedData == null)
-        {
-            this._sparklineMixedData = new SparklineMixedData();
-        }
-        return this._sparklineMixedData;
-    }
-    
 
     private _componentRenderer: ComponentRenderer = null;
     public get renderer(): ComponentRenderer {
