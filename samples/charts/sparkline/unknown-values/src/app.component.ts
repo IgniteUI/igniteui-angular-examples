5bf2ce81749fbe5e7a5047459f100d08ca800b57
tfsbuild <tfsbuild@infragistics.com>
2023-04-13 05:07:05 -0400

    Adding changes from build IgniteUI.igniteui-xplat-examples_2023.4.13.9

--- samples/charts/sparkline/unknown-values/src/app.component.ts
+++ samples/charts/sparkline/unknown-values/src/app.component.ts
@@ -1,12 +1,9 @@
 import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ComponentRenderer, PropertyEditorPanelDescriptionModule, SparklineDescriptionModule } from 'igniteui-angular-core';
-import { SparklineUnknownDataItem, SparklineUnknownData } from './SparklineUnknownData';
-import { IgxPropertyEditorPanelComponent, IgxPropertyEditorPropertyDescriptionComponent } from 'igniteui-angular-layouts';
-import { IgxSparklineComponent } from 'igniteui-angular-charts';
+//insert bindingImports
+//end bindingImports
 
-import { defineAllComponents } from 'igniteui-webcomponents';
 
-defineAllComponents();
 
 @Component({
     selector: "app-root",
@@ -20,22 +17,9 @@ export class AppComponent {
 
     }
 
-    @ViewChild("propertyEditorPanel1", { static: true } )
-    private propertyEditorPanel1: IgxPropertyEditorPanelComponent
-    @ViewChild("unknownValuePlottingEditor", { static: true } )
-    private unknownValuePlottingEditor: IgxPropertyEditorPropertyDescriptionComponent
-    @ViewChild("chart", { static: true } )
-    private chart: IgxSparklineComponent
+    //insert bindingFields
+    //end bindingFields
 
-    private _sparklineUnknownData: SparklineUnknownData = null;
-    public get sparklineUnknownData(): SparklineUnknownData {
-        if (this._sparklineUnknownData == null)
-        {
-            this._sparklineUnknownData = new SparklineUnknownData();
-        }
-        return this._sparklineUnknownData;
-    }
-    
 
     private _componentRenderer: ComponentRenderer = null;
     public get renderer(): ComponentRenderer {
