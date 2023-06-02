d9eb797747ae3bfe866e438eb4c26c0e38f4c866
HUSSAR-mtrela <martin.trela@gmail.com>
2023-06-02 14:15:32 -0400

    add theming sample

---
+++ samples/charts/financial-chart/theming/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, NgZone, OnDestroy, ViewChild } from "@angular/core";
+import { FinancialChartType } from "igniteui-angular-charts";
+import { FinancialChartZoomSliderType } from "igniteui-angular-charts";
+import { FinancialOverlayType } from "igniteui-angular-charts";
+import { IgxFinancialChartComponent } from "igniteui-angular-charts";
+import { StockDataService } from "./StockDataService";
+
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [ StockDataService ],
+    selector: "app-root",
+    styleUrls: ["./app.component.scss",
+    "./dark-theme-financial-chart.css",
+    "./dark-theme-legend.css",
+    "./dark-theme-tooltips.css",
+    "./dark-theme-zoom-slider.css"
+    ],
+    templateUrl: "./app.component.html"
+})
+
+export class AppComponent implements AfterViewInit {
+
+    public data: any[];
+    @ViewChild("chart", { static: true })
+    public chart: IgxFinancialChartComponent;
+
+    constructor(private dataService: StockDataService) {
+        this.data = this.dataService.GetStockTSLA();
+
+    }
+
+    public ngAfterViewInit(): void {
+
+        this.chart.chartType = FinancialChartType.Candle;
+        this.chart.zoomSliderType = FinancialChartZoomSliderType.Candle;
+        this.chart.overlayTypes.add(FinancialOverlayType.PriceChannel);
+    }
+}
