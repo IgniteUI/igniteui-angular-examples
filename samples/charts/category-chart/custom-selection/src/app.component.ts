20993dee947534f2f40ef4e10367448414c0d4a9
HUSSAR-mtrela <martin.trela@gmail.com>
2023-10-18 14:14:36 -0400

    export new samples

---
+++ samples/charts/category-chart/custom-selection/src/app.component.ts
@@ -0,0 +1,61 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { SelectableDataItem, SelectableData } from './SelectableData';
+import { IgxDataLegendComponent, IgxCategoryChartComponent } from 'igniteui-angular-charts';
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+
+    public constructor(private _detector: ChangeDetectorRef) {
+
+    }
+
+    @ViewChild("legend", { static: true } )
+    private legend: IgxDataLegendComponent
+    @ViewChild("chart", { static: true } )
+    private chart: IgxCategoryChartComponent
+
+    private _selectableData: SelectableData = null;
+    public get selectableData(): SelectableData {
+        if (this._selectableData == null)
+        {
+            this._selectableData = new SelectableData();
+        }
+        return this._selectableData;
+    }
+
+
+    public categoryChartCustomSelectionPointerDown(e: any): void {
+
+        let oldItem = e.args.item as SelectableDataItem;
+
+        if (oldItem === null) return;
+
+        let newItem: SelectableDataItem = new SelectableDataItem({
+            category: oldItem.category,
+            dataValue: oldItem.dataValue,
+            selectedValue: oldItem.selectedValue
+        });
+
+        var selectedIndex = -1;
+        for (var i = 0; i < this.selectableData.length; i++) {
+            if (oldItem.category === this.selectableData[i].category) {
+                selectedIndex = i;
+                break;
+            }
+        }
+
+        if (oldItem.selectedValue === oldItem.dataValue)
+            newItem.selectedValue = null;
+        else
+            newItem.selectedValue = newItem.dataValue;
+
+        this.chart.notifySetItem(this.selectableData, selectedIndex, oldItem, newItem);
+    }
+
+}
+
---
+++ samples/charts/data-chart/format-specifiers/src/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+import { IgxDataLegendModule, IgxDataChartCoreModule, IgxDataChartCategoryCoreModule, IgxDataChartCategoryModule, IgxDataChartInteractivityModule, IgxDataChartVerticalCategoryModule, IgxDataChartAnnotationModule } from 'igniteui-angular-charts';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    IgxDataLegendModule,
+    IgxDataChartCoreModule,
+    IgxDataChartCategoryCoreModule,
+    IgxDataChartCategoryModule,
+    IgxDataChartInteractivityModule,
+    IgxDataChartVerticalCategoryModule,
+    IgxDataChartAnnotationModule
+],
+  providers: [],
+  schemas: []
+})
+export class AppModule {}
