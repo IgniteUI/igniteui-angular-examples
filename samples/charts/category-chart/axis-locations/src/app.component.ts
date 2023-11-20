34995de83322688b907848075748002960ad6c51
tfsbuild <tfsbuild@infragistics.com>
2023-11-20 17:21:39 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.11.20.1

---
+++ samples/charts/category-chart/axis-locations/src/app.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, LegendDescriptionModule, CategoryChartDescriptionModule } from 'igniteui-angular-core';
+import { CountryRenewableElectricityItem, CountryRenewableElectricity } from './CountryRenewableElectricity';
+import { IgxLegendComponent, IgxCategoryChartComponent } from 'igniteui-angular-charts';
+import { IgxPropertyEditorPanelComponent, IgxPropertyEditorPropertyDescriptionComponent } from 'igniteui-angular-layouts';
+
+import { defineAllComponents } from 'igniteui-webcomponents';
+
+defineAllComponents();
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+
+    public constructor(private _detector: ChangeDetectorRef) {
+
+    }
+
+    @ViewChild("legend", { static: true } )
+    private legend: IgxLegendComponent
+    @ViewChild("propertyEditorPanel1", { static: true } )
+    private propertyEditorPanel1: IgxPropertyEditorPanelComponent
+    @ViewChild("yAxisLabelLocation", { static: true } )
+    private yAxisLabelLocation: IgxPropertyEditorPropertyDescriptionComponent
+    @ViewChild("chart", { static: true } )
+    private chart: IgxCategoryChartComponent
+
+    private _countryRenewableElectricity: CountryRenewableElectricity = null;
+    public get countryRenewableElectricity(): CountryRenewableElectricity {
+        if (this._countryRenewableElectricity == null)
+        {
+            this._countryRenewableElectricity = new CountryRenewableElectricity();
+        }
+        return this._countryRenewableElectricity;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            LegendDescriptionModule.register(context);
+            CategoryChartDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
