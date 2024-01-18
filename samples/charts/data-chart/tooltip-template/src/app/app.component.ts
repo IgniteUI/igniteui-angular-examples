c93e207758d3d7e4df2398cbfc2dde4921acec30
mddifilippo89 <mdifilippo@infragistics.com>
2024-01-18 11:37:04 -0500

    mdd-update-ig
    mdd-update-ig
    17.2.1

--- samples/charts/data-chart/tooltip-template/src/app/app.component.ts
+++ samples/charts/data-chart/tooltip-template/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { UnknownValuePlotting } from "igniteui-angular-core";
 
 @Component({
     selector: "app-root",
@@ -8,12 +9,13 @@ import { Component } from "@angular/core";
 export class AppComponent {
 
     public data: any;
-
+    public unknownValuePlotting: UnknownValuePlotting = UnknownValuePlotting.DontPlot;
+    
     constructor() {
         this.data = [
             { Country: "Canada", Coal: 400, Oil: 100, Gas: 175, Nuclear: 225, Hydro: 350 },
-            { Country: "China", Coal: 925, Oil: 200, Gas: 350, Nuclear: 400, Hydro: 625 },
-            { Country: "Russia", Coal: 550, Oil: 200, Gas: 250, Nuclear: 475, Hydro: 425 },
+            { Country: "China", Coal: 925, Oil: null, Gas: null, Nuclear: 400, Hydro: 625 },
+            { Country: "Russia", Coal: 550, Oil: null, Gas: null, Nuclear: 475, Hydro: 425 },
             { Country: "Australia", Coal: 450, Oil: 100, Gas: 150, Nuclear: 175, Hydro: 350 },
             { Country: "United States", Coal: 800, Oil: 250, Gas: 475, Nuclear: 575, Hydro: 750 },
             { Country: "France", Coal: 375, Oil: 150, Gas: 350, Nuclear: 275, Hydro: 325 }
