2843c0496dfd4031ddc194a9d57e2bc9bb72d920
tfsbuild <tfsbuild@infragistics.com>
2024-03-05 10:21:34 -0500

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.3.5.1

---
+++ samples/charts/data-chart/data-legend-styling/src/app.component.ts
@@ -0,0 +1,62 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ComponentRenderer, LegendDescriptionModule, NumberAbbreviatorDescriptionModule, DataChartCoreDescriptionModule, DataChartCategoryDescriptionModule, DataChartInteractivityDescriptionModule, DataLegendDescriptionModule, DataChartAnnotationDescriptionModule } from 'igniteui-angular-core';
+import { OlympicMedalsTopCountriesWithTotalsItem, OlympicMedalsTopCountriesWithTotals } from './OlympicMedalsTopCountriesWithTotals';
+import { IgxDataLegendComponent, IgxDataChartComponent, IgxCategoryXAxisComponent, IgxNumericYAxisComponent, IgxColumnSeriesComponent, IgxDataToolTipLayerComponent } from 'igniteui-angular-charts';
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AppComponent implements AfterViewInit
+{
+
+	@ViewChild("legend", { static: true } )
+	private legend: IgxDataLegendComponent
+	@ViewChild("chart", { static: true } )
+	private chart: IgxDataChartComponent
+	@ViewChild("xAxis", { static: true } )
+	private xAxis: IgxCategoryXAxisComponent
+	@ViewChild("yAxis", { static: true } )
+	private yAxis: IgxNumericYAxisComponent
+	@ViewChild("columnSeries1", { static: true } )
+	private columnSeries1: IgxColumnSeriesComponent
+	@ViewChild("dataToolTipLayer", { static: true } )
+	private dataToolTipLayer: IgxDataToolTipLayerComponent
+    private _olympicMedalsTopCountriesWithTotals: OlympicMedalsTopCountriesWithTotals = null;
+    public get olympicMedalsTopCountriesWithTotals(): OlympicMedalsTopCountriesWithTotals {
+        if (this._olympicMedalsTopCountriesWithTotals == null)
+        {
+            this._olympicMedalsTopCountriesWithTotals = new OlympicMedalsTopCountriesWithTotals();
+        }
+        return this._olympicMedalsTopCountriesWithTotals;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            LegendDescriptionModule.register(context);
+            NumberAbbreviatorDescriptionModule.register(context);
+            DataChartCoreDescriptionModule.register(context);
+            DataChartCategoryDescriptionModule.register(context);
+            DataChartInteractivityDescriptionModule.register(context);
+            DataLegendDescriptionModule.register(context);
+            DataChartAnnotationDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+	public constructor(private _detector: ChangeDetectorRef)
+	{
+	}
+
+	public ngAfterViewInit(): void
+	{
+	}
+
+}
+
