88219a0c5ffbe0fbf6c8129e3a06e328c00b5ece
HUSSAR-mtrela <HUSSAR-mtrela@users.noreply.github.com>
2024-06-03 15:32:08 -0400

    test avatar

---
+++ tests/avatar-sample-3/src/app/avatar-sample-3/avatar-sample-3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-sample-3',
+  styleUrls: ['./avatar-sample-3.component.scss'],
+  templateUrl: './avatar-sample-3.component.html'
+})
+export class AvatarSample3Component {
+
+  constructor() { }
+
+}
