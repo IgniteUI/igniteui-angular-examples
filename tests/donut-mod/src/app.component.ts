feb6cce331cab08bcfb84bba0ef8eab6e8d1126b
HUSSAR-mtrela <HUSSAR-mtrela@users.noreply.github.com>
2024-06-05 10:54:08 -0400

    update CS tests

---
+++ tests/donut-mod/src/app.component.ts
@@ -0,0 +1,39 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { EnergyGlobalDemandItem, EnergyGlobalDemand } from './EnergyGlobalDemand';
+import { IgxItemLegendComponent, IgxDoughnutChartComponent, IgxRingSeriesComponent } from 'igniteui-angular-charts';
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AppComponent implements AfterViewInit
+{
+
+	@ViewChild("legend", { static: true } )
+	private legend: IgxItemLegendComponent
+	@ViewChild("chart", { static: true } )
+	private chart: IgxDoughnutChartComponent
+	@ViewChild("series", { static: true } )
+	private series: IgxRingSeriesComponent
+    private _energyGlobalDemand: EnergyGlobalDemand = null;
+    public get energyGlobalDemand(): EnergyGlobalDemand {
+        if (this._energyGlobalDemand == null)
+        {
+            this._energyGlobalDemand = new EnergyGlobalDemand();
+        }
+        return this._energyGlobalDemand;
+    }
+
+	public constructor(private _detector: ChangeDetectorRef)
+	{
+	}
+
+	public ngAfterViewInit(): void
+	{
+	}
+
+}
+
