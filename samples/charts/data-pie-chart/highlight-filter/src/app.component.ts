388fb3525aebc8c6dfdae4d8b77176a40cbb942b
tfsbuild <tfsbuild@infragistics.com>
2024-08-22 10:38:30 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.8.22.1

---
+++ samples/charts/data-pie-chart/highlight-filter/src/app.component.ts
@@ -0,0 +1,56 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ComponentRenderer, DataPieChartDescriptionModule } from 'igniteui-angular-core';
+import { OnlineTrafficHighlightTotalsItem, OnlineTrafficHighlightTotals } from './OnlineTrafficHighlightTotals';
+import { OnlineTrafficHighlightDesktopOnlyItem, OnlineTrafficHighlightDesktopOnly } from './OnlineTrafficHighlightDesktopOnly';
+import { IgxDataPieChartComponent } from 'igniteui-angular-core';
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AppComponent implements AfterViewInit
+{
+
+	@ViewChild("chart", { static: true } )
+	private chart: IgxDataPieChartComponent
+    private _onlineTrafficHighlightTotals: OnlineTrafficHighlightTotals = null;
+    public get onlineTrafficHighlightTotals(): OnlineTrafficHighlightTotals {
+        if (this._onlineTrafficHighlightTotals == null)
+        {
+            this._onlineTrafficHighlightTotals = new OnlineTrafficHighlightTotals();
+        }
+        return this._onlineTrafficHighlightTotals;
+    }
+
+    private _onlineTrafficHighlightDesktopOnly: OnlineTrafficHighlightDesktopOnly = null;
+    public get onlineTrafficHighlightDesktopOnly(): OnlineTrafficHighlightDesktopOnly {
+        if (this._onlineTrafficHighlightDesktopOnly == null)
+        {
+            this._onlineTrafficHighlightDesktopOnly = new OnlineTrafficHighlightDesktopOnly();
+        }
+        return this._onlineTrafficHighlightDesktopOnly;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            DataPieChartDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+	public constructor(private _detector: ChangeDetectorRef)
+	{
+	}
+
+	public ngAfterViewInit(): void
+	{
+	}
+
+}
+
