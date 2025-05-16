f31c7cd26b374ddab3bae35c009bc343813906e5
tfsbuild <tfsbuild@infragistics.com>
2025-05-16 06:12:18 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.5.16.1

---
+++ samples/charts/data-chart/data-annotation-rect-layer/src/app.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { AnnotationDataItem, AnnotationData } from './SampleData';
+import { StockTeslaItem, StockTesla } from './StockTesla';
+import { IgxDataChartComponent, IgxCategoryXAxisComponent, IgxNumericYAxisComponent, IgxFinancialPriceSeriesComponent, IgxDataToolTipLayerComponent } from 'igniteui-angular-charts';
+
+@Component({
+    standalone: false,
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AppComponent implements AfterViewInit
+{
+
+	@ViewChild("chart", { static: true } )
+	private chart: IgxDataChartComponent
+	@ViewChild("xAxis", { static: true } )
+	private xAxis: IgxCategoryXAxisComponent
+	@ViewChild("yAxis", { static: true } )
+	private yAxis: IgxNumericYAxisComponent
+	@ViewChild("series1", { static: true } )
+	private series1: IgxFinancialPriceSeriesComponent
+	@ViewChild("tooltip", { static: true } )
+	private tooltip: IgxDataToolTipLayerComponent
+    private _annotationData: AnnotationData = null;
+    public get annotationData(): AnnotationData {
+        if (this._annotationData == null)
+        {
+            this._annotationData = new AnnotationData();
+        }
+        return this._annotationData;
+    }
+
+    private _stockTesla: StockTesla = null;
+    public get stockTesla(): StockTesla {
+        if (this._stockTesla == null)
+        {
+            this._stockTesla = new StockTesla();
+        }
+        return this._stockTesla;
+    }
+
+	public constructor(private _detector: ChangeDetectorRef)
+	{
+	}
+
+	public ngAfterViewInit(): void
+	{
+	}
+
+}
+
