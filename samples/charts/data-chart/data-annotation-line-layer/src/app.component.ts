9c92d06972ad1ceb546aa300a66454cb314c22b0
tfsbuild <tfsbuild@infragistics.com>
2025-05-20 10:43:20 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.5.20.4

---
+++ samples/charts/data-chart/data-annotation-line-layer/src/app.component.ts
@@ -0,0 +1,69 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { AnnotationData1Item, AnnotationData1, AnnotationData2Item, AnnotationData2 } from './SampleData';
+import { StockTeslaItem, StockTesla } from './StockTesla';
+import { IgxDataChartComponent, IgxCategoryXAxisComponent, IgxNumericYAxisComponent, IgxFinancialPriceSeriesComponent, IgxDataToolTipLayerComponent, IgxDataAnnotationLineLayerComponent } from 'igniteui-angular-charts';
+
+@Component({
+    standalone: false,
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AppComponent implements AfterViewInit
+{
+
+	@ViewChild("chart", { static: true } )
+	private chart: IgxDataChartComponent
+	@ViewChild("xAxis", { static: true } )
+	private xAxis: IgxCategoryXAxisComponent
+	@ViewChild("yAxisLeft", { static: true } )
+	private yAxisLeft: IgxNumericYAxisComponent
+	@ViewChild("yAxisRight", { static: true } )
+	private yAxisRight: IgxNumericYAxisComponent
+	@ViewChild("series1", { static: true } )
+	private series1: IgxFinancialPriceSeriesComponent
+	@ViewChild("tooltip", { static: true } )
+	private tooltip: IgxDataToolTipLayerComponent
+	@ViewChild("lineLayer52WeekRange", { static: true } )
+	private lineLayer52WeekRange: IgxDataAnnotationLineLayerComponent
+	@ViewChild("lineLayerGrowthAndDecline", { static: true } )
+	private lineLayerGrowthAndDecline: IgxDataAnnotationLineLayerComponent
+    private _annotationData1: AnnotationData1 = null;
+    public get annotationData1(): AnnotationData1 {
+        if (this._annotationData1 == null)
+        {
+            this._annotationData1 = new AnnotationData1();
+        }
+        return this._annotationData1;
+    }
+
+    private _annotationData2: AnnotationData2 = null;
+    public get annotationData2(): AnnotationData2 {
+        if (this._annotationData2 == null)
+        {
+            this._annotationData2 = new AnnotationData2();
+        }
+        return this._annotationData2;
+    }
+
+    private _stockTesla: StockTesla = null;
+    public get stockTesla(): StockTesla {
+        if (this._stockTesla == null)
+        {
+            this._stockTesla = new StockTesla();
+        }
+        return this._stockTesla;
+    }
+
+	public constructor(private _detector: ChangeDetectorRef)
+	{
+	}
+
+	public ngAfterViewInit(): void
+	{
+	}
+
+}
+
---
+++ samples/charts/data-chart/data-annotation-multiple-with-overlay-text/src/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+import { IgxDataChartCoreModule, IgxDataChartCategoryModule, IgxDataChartCategoryCoreModule, IgxDataChartInteractivityModule, IgxAnnotationLayerProxyModule, IgxDataChartAnnotationModule, IgxDataAnnotationSliceLayerModule, IgxNumberAbbreviatorModule, IgxValueOverlayModule } from 'igniteui-angular-charts';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    IgxDataChartCoreModule,
+    IgxDataChartCategoryModule,
+    IgxDataChartCategoryCoreModule,
+    IgxDataChartInteractivityModule,
+    IgxAnnotationLayerProxyModule,
+    IgxDataChartAnnotationModule,
+    IgxDataAnnotationSliceLayerModule,
+    IgxNumberAbbreviatorModule,
+    IgxAnnotationLayerProxyModule,
+    IgxValueOverlayModule
+],
+  providers: [],
+  schemas: []
+})
+export class AppModule {}
