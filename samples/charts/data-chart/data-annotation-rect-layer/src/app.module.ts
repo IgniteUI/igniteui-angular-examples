53b6d213e77bbba3d3af0d598d96f4427b32c3b1
tfsbuild <tfsbuild@infragistics.com>
2025-05-23 00:10:48 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.5.22.1

---
+++ samples/charts/data-chart/data-annotation-rect-layer/src/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+import { IgxDataChartCoreModule, IgxDataChartCategoryModule, IgxDataChartCategoryCoreModule, IgxDataChartFinancialCoreModule, IgxDataChartFinancialModule, IgxDataChartFinancialOverlaysModule, IgxDataChartInteractivityModule, IgxDataChartAnnotationModule, IgxDataAnnotationRectLayerModule, IgxNumberAbbreviatorModule, IgxAnnotationLayerProxyModule } from 'igniteui-angular-charts';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    IgxDataChartCoreModule,
+    IgxDataChartCategoryModule,
+    IgxDataChartCategoryCoreModule,
+    IgxDataChartFinancialCoreModule,
+    IgxDataChartFinancialModule,
+    IgxDataChartFinancialOverlaysModule,
+    IgxDataChartInteractivityModule,
+    IgxDataChartAnnotationModule,
+    IgxDataAnnotationRectLayerModule,
+    IgxNumberAbbreviatorModule,
+    IgxAnnotationLayerProxyModule
+],
+  providers: [],
+  schemas: []
+})
+export class AppModule {}
