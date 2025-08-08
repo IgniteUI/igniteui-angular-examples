9f60c0ae9f9da519130e4bfdab805aee4fff7546
HUSSAR-mtrela (Martin Trela) <HUSSAR-mtrela@users.noreply.github.com>
2025-08-08 14:25:56 -0400

    export-code-gen-fixes (#345)

---
+++ samples/charts/data-chart/trendline-layer/src/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+import { IgxDataChartCategoryModule, IgxDataChartInteractivityModule, IgxDataChartAnnotationModule, IgxDataChartCategoryTrendLineModule, IgxTrendLineLayerModule, IgxDataLegendModule } from 'igniteui-angular-charts';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    IgxDataChartCategoryModule,
+    IgxDataChartInteractivityModule,
+    IgxDataChartAnnotationModule,
+    IgxDataChartCategoryTrendLineModule,
+    IgxTrendLineLayerModule,
+    IgxDataLegendModule
+],
+  providers: [],
+  schemas: []
+})
+export class AppModule {}
