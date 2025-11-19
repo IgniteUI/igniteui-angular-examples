a87562e5642f99b525e9add6c6d736f7f9b7d467
mddifilippo89 <mdifilippo@infragistics.com>
2025-11-19 15:34:08 -0500

    mdd-update-bing-for-enterprise-key (#370)
    * mdd-update-bing-for-enterprise-key
    
    mdd-update-bing-for-enterprise-key
    
    * mdd-update
    
    mdd-update
    
    * mdd-revert-bg-package
    
    mdd-revert-bg-package

--- samples/maps/geo-map/display-bing-imagery/src/app.module.ts
+++ samples/maps/geo-map/display-bing-imagery/src/app.module.ts
@@ -1,27 +1,37 @@
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
-import { IgxGeographicMapModule } from "igniteui-angular-maps";
+
+// Ignite UI Maps & Charts
+import { IgxAzureMapsImageryModule, IgxGeographicMapModule } from "igniteui-angular-maps";
 import { IgxDataChartInteractivityModule } from "igniteui-angular-charts";
 
+// Ignite UI Angular components
+import { IgxButtonModule, IgxDialogModule, IgxIconModule, IgxInputGroupModule, IgxSelectModule } from "igniteui-angular";
 
 @NgModule({
-  bootstrap: [AppComponent],
   declarations: [
     AppComponent
-],
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
     IgxGeographicMapModule,
-    IgxDataChartInteractivityModule
-],
+    IgxAzureMapsImageryModule,
+    IgxDataChartInteractivityModule,
+    IgxDialogModule,
+    IgxInputGroupModule,
+    IgxIconModule,
+    IgxButtonModule,
+    IgxSelectModule       // <-- Add this for the dropdown combo
+  ],
   providers: [],
-  schemas: []
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // <-- Needed to allow Web Components like igx-combo
 })
 export class AppModule {}
